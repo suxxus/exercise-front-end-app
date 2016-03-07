046b8180341b433dd1079bc8ffc0edfb3219f04b
diego <diegoriez@gmail.com>
2016-03-07 17:06:14 +0100

    refactor test

--- tests/src/store/configure-store.spec.js
+++ tests/src/store/configure-store.spec.js
@@ -2,18 +2,36 @@ import R from 'ramda';
 import test from 'blue-tape';
 import mock from 'mock';
 import * as Mocked from '../../mocks/mock';
-import ConfigureStore from 'scripts/store/configure-store';
 
-test('ConfigureStore', t => {
 
-    const actual = typeof ConfigureStore(),
-        expect = 'object';
+const ConfigureStore = mock('scripts/store/configure-store', {
+            'scripts/reducers': () => ({metrics:''})
+        },
+        require).default,
 
-    t.deepEqual(actual, expect, 'should be an object');
+    before = test,
+    after = test;
+
+before('desc:', t => {
+    t.pass('store: configure store');
     t.end();
 });
 
-test('end', t => {
+test('ConfigureStore', t => {
+
+    let actual, expect;
+    const store = ConfigureStore();
+
+    actual = typeof store.getState();
+    expect = 'object';
+    t.equal(actual, expect, 'configured with the correct reducer');
+
+    t.deepEqual(actual, expect);
+
+    t.end();
+});
+
+after('end', t => {
     t.pass('//--------------------------------------');
     t.end();
 });
